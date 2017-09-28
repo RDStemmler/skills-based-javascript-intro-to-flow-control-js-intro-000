7fb1563be356108da47b61b90a6a56925a77ac0e
Rob <rdstemmler@gmail.com>
2017-09-28 18:23:40 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -25,5 +25,20 @@ return "You are a teenager";
 }
 
 function switchAge(age) {
-
+  switch(age){
+    case 13: 
+    return('You are a teenager')
+    case 14: 
+    return('You are a teenager')
+    case 15: 
+    return('You are a teenager')
+    case 16: 
+    return('You are a teenager')
+    case 17: 
+    return('You are a teenager')
+    case 18: 
+    return('You are a teenager')
+    case 19: 
+    return('You are a teenager')
+  }
 }
