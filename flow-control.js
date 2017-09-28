14bf65b9f372c0a6d5dde1b5fdf655fcfdf4fd8c
Rob <rdstemmler@gmail.com>
2017-09-28 18:06:11 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -27,7 +27,7 @@ function ternaryTeenager(age) {
   if(age >= 13 && age <= 19) {
     return('You are a teenager!')
   } else {
-  if(age < 13 && age > 19) {
+  if(age < 19 && age > 13) {
     return('You are not a teenager!')
   }
 }
